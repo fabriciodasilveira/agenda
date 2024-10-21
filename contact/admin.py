c6c09c1336396cae275e70efc4372975967fc452
Fabricio Silveira <fabvass@br.ibm.com>
2024-10-21 20:35:04 -0300

    upt

--- contact/admin.py
+++ contact/admin.py
@@ -5,10 +5,14 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','last_name', 'phone',)
+    list_display = ('id','first_name','last_name', 'phone','category')
     ordering = ('-id',)
-    # list_filter = ('created_date',)
+    list_filter = ('category',)
     search_fields = ('first_name','last_name',)
     list_per_page = 10
     list_max_show_all = 100
     list_display_links = ('id', 'first_name',)
+    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,18 @@ STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
 
+STATIC_ROOT = BASE_DIR / 'static' #collect statics
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...
